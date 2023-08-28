bbf8c1af4ac1092e0979308545dc4c08d316a327
Artur <lozinski.artur@gmail.com>
2023-08-28 15:20:34 +0200

    add test facility

---
+++ pytest/GTestResult/gtest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include "mainMockMock.h"
+
+extern "C" {
+#include <stdio.h>
+	void foo()
+	{
+		printf( "ole\n" );
+	}
+}
+
+
+TEST(abc, xyz ) {
+	p_mainMock = new MainmockMock;
+	EXPECT_CALL( *p_mainMock, init_by_pFunc(foo) ).WillOnce( ::testing::Return(1) );
+
+	EXPECT_EQ(init_by_pFunc(foo), 2 );
+	delete p_mainMock;
+}
